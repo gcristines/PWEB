8c1644e36510166263f76837319c1fc77b171d9a
Gisele Santos <86169220+gcristines@users.noreply.github.com>
2025-04-09 21:50:34 -0300

    Add files via upload

---
+++ Atividade8/Atividade8.js
@@ -0,0 +1,163 @@
+var idade, vetorIdade = [];
+var sexo, vetorSexo = [];
+var opiniao, vetorOpiniao = [];
+var pessoa = 0;
+
+
+while (pessoa < 5) {
+
+    idade = parseInt(prompt("Qual a sua idade?"));
+    vetorIdade.push(idade);
+
+    sexo = prompt("Qual seu sexo? Informe F ou M.");
+    vetorSexo.push(sexo);
+
+    opiniao = parseInt(prompt("Sua opinião sobre o filme é: ÓTIMO = 4, BOM = 3, REGULAR = 2 ou PÉSSIMO = 1 ?"));
+    vetorOpiniao.push(opiniao);
+
+    pessoa ++;
+
+}
+
+function mediaIdade(vetorIdade) {
+
+    let somarIdade = 0;
+
+    for (let i = 0; i < 5; i++) {
+        somarIdade += vetorIdade[i];
+    }
+
+    return (somarIdade / pessoa);
+}
+
+alert("A média da idade das pessoas que responderam ao questionário é: " + mediaIdade(vetorIdade));
+
+
+function maiorIdade(vetorIdade) {
+   
+    let maior = vetorIdade[0];
+
+    for (let i = 1; i < 5; i++) {
+        
+        if (vetorIdade[i] > maior) {
+            maior = vetorIdade[i];
+        }
+
+    }
+
+    return maior;
+}
+
+alert("A idade da pessoa mais velha é: " + maiorIdade(vetorIdade));
+
+
+function menorIdade(vetorIdade) {
+   
+    let menor = vetorIdade[0];
+
+    for (let i = 1; i < 5; i++) {
+        
+        if (vetorIdade[i] < menor) {
+            menor = vetorIdade[i];
+        }
+
+    }
+
+    return menor;
+}
+
+alert("A idade da pessoa mais nova é: " + menorIdade(vetorIdade));
+
+
+function filmePessimo(vetorOpiniao) {
+
+    let contPessimo = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+        if (vetorOpiniao[i] == 1) {
+            contPessimo ++;
+        }
+
+    }
+    return contPessimo;
+
+}
+
+alert("A quantidade de pessoas que responderam péssimo é: " + filmePessimo(vetorOpiniao));
+
+
+function filmeOtimo(vetorOpiniao) {
+
+    let contOtimo = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+        if (vetorOpiniao[i] == 4) {
+            contOtimo ++;
+        }
+
+    }
+
+    return ((contOtimo / pessoa) * 100);    
+
+}
+
+alert("A porcentagem de pessoas que responderam ótimo é: " + filmeOtimo(vetorOpiniao) + "%");
+
+
+function filmeBom(vetorOpiniao) {
+
+    let contBom = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        if (vetorOpiniao[i] == 3) {
+            contBom ++;
+        }
+
+    }
+
+    return ((contBom / pessoa) * 100);
+
+}
+
+alert("A porcentagem de pessoas que responderam bom é: " + filmeBom(vetorOpiniao) + "%");
+
+
+function contMulher(vetorSexo) {
+
+    let contMulher = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+        if (vetorSexo[i] == 'F') {
+            contMulher ++;
+        }
+
+    }
+
+    return contMulher;
+
+}
+
+alert("O número de mulheres que responderam ao questionário é: " + contMulher(vetorSexo));
+
+
+function contHomem(vetorSexo) {
+
+    let contHomem = 0;
+
+    for (let i = 0; i < 5; i++) {
+        
+        if (vetorSexo[i] == 'M') {
+            contHomem ++;
+        }
+
+    }
+
+    return contHomem;
+    
+}
+
+alert("O número de homens que responderam ao questionário é: " + contHomem(vetorSexo));
